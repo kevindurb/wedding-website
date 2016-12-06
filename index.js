1d2edc9318faeb124ecc11cbe9a6eea131ab0e69
Kevin Durbin <kevindurb@gmail.com>
2016-12-05 20:31:48 -0700

    remove arrows on scroll

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+function hideDownArrow() {
+  var arrow = document.querySelector('.hero .arrow');
+  arrow.classList.add('hide');
+}
+
+function hideGalleryArrow() {
+  var arrow = document.querySelector('.right-arrow');
+  arrow.classList.add('hide');
+}
+
 function init() {
   console.log('init');
+  var gallery = document.querySelector('.images');
+
+  document.addEventListener('scroll', hideDownArrow);
+
+  gallery.addEventListener('scroll', hideGalleryArrow);
 };
 
 window.addEventListener('DOMContentLoaded', init, false);
